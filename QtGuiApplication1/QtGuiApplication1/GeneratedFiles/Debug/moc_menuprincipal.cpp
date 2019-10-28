6e19a3b4fc878ad53c13a63f818a5aaee7fbf6b2
Eduardo <edr.ccol@gmail.com>
2019-10-28 11:31:46 -0300

    Add files via upload

--- QtGuiApplication1/QtGuiApplication1/GeneratedFiles/Debug/moc_menuprincipal.cpp
+++ QtGuiApplication1/QtGuiApplication1/GeneratedFiles/Debug/moc_menuprincipal.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_menuPrincipal_t {
-    QByteArrayData data[3];
-    char stringdata0[39];
+    QByteArrayData data[11];
+    char stringdata0[178];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,11 +33,23 @@ static const qt_meta_stringdata_menuPrincipal_t qt_meta_stringdata_menuPrincipal
     {
 QT_MOC_LITERAL(0, 0, 13), // "menuPrincipal"
 QT_MOC_LITERAL(1, 14, 23), // "on_botaoInserir_clicked"
-QT_MOC_LITERAL(2, 38, 0) // ""
+QT_MOC_LITERAL(2, 38, 0), // ""
+QT_MOC_LITERAL(3, 39, 22), // "on_botaoListar_clicked"
+QT_MOC_LITERAL(4, 62, 25), // "on_botaoConsultar_clicked"
+QT_MOC_LITERAL(5, 88, 23), // "on_botaoExcluir_clicked"
+QT_MOC_LITERAL(6, 112, 23), // "on_botaoCompras_clicked"
+QT_MOC_LITERAL(7, 136, 21), // "on_botaoVenda_clicked"
+QT_MOC_LITERAL(8, 158, 9), // "fill_hash"
+QT_MOC_LITERAL(9, 168, 7), // "Produto"
+QT_MOC_LITERAL(10, 176, 1) // "p"
 
     },
     "menuPrincipal\0on_botaoInserir_clicked\0"
-    ""
+    "\0on_botaoListar_clicked\0"
+    "on_botaoConsultar_clicked\0"
+    "on_botaoExcluir_clicked\0on_botaoCompras_clicked\0"
+    "on_botaoVenda_clicked\0fill_hash\0Produto\0"
+    "p"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +59,7 @@ static const uint qt_meta_data_menuPrincipal[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,10 +67,22 @@ static const uint qt_meta_data_menuPrincipal[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x08 /* Private */,
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    0,   52,    2, 0x08 /* Private */,
+       6,    0,   53,    2, 0x08 /* Private */,
+       7,    0,   54,    2, 0x08 /* Private */,
+       8,    1,   55,    2, 0x0a /* Public */,
 
  // slots: parameters
-    QMetaType::Int,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 9,   10,
 
        0        // eod
 };
@@ -69,8 +93,13 @@ void menuPrincipal::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         auto *_t = static_cast<menuPrincipal *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: { int _r = _t->on_botaoInserir_clicked();
-            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 0: _t->on_botaoInserir_clicked(); break;
+        case 1: _t->on_botaoListar_clicked(); break;
+        case 2: _t->on_botaoConsultar_clicked(); break;
+        case 3: _t->on_botaoExcluir_clicked(); break;
+        case 4: _t->on_botaoCompras_clicked(); break;
+        case 5: _t->on_botaoVenda_clicked(); break;
+        case 6: _t->fill_hash((*reinterpret_cast< Produto(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -96,8 +125,6 @@ void *menuPrincipal::qt_metacast(const char *_clname)
     if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_menuPrincipal.stringdata0))
         return static_cast<void*>(this);
-    if (!strcmp(_clname, "Ui::menuPrincipal"))
-        return static_cast< Ui::menuPrincipal*>(this);
     return QDialog::qt_metacast(_clname);
 }
 
@@ -107,13 +134,13 @@ int menuPrincipal::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 7;
     }
     return _id;
 }
